f1d149dba618a3b17803baf2a98b9a9f583b98ad
DhanBirSunwar <45319595+DhanBirSunwar@users.noreply.github.com>
2023-05-08 13:05:32 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# manage-multilingual-content-in-storyblok-and-next-gcjs71
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/manage-multilingual-content-in-storyblok-and-next-gcjs71)
---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+import '../styles/tailwind.css';
+import { storyblokInit, apiPlugin } from '@storyblok/react';
+import Feature from '../components/Feature';
+import Grid from '../components/Grid';
+import Page from '../components/Page';
+import Teaser from '../components/Teaser';
+import Hero from '../components/Hero';
+import AllArticles from '../components/AllArticles';
+import Article from '../components/Article';
+import PopularArticles from '../components/PopularArticles';
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+  hero: Hero,
+  'all-articles': AllArticles,
+  article: Article,
+  'popular-articles': PopularArticles,
+};
+
+storyblokInit({
+  accessToken: 'jURWpRXIYKl2MB8kmG9WCQtt',
+  // for spaces located in the US or China:
+  // apiOptions: {
+  //   region: "us" or "cn", // you need to specify the region
+  // },
+  use: [apiPlugin],
+  components,
+});
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
